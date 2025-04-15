eaee38e63d5af6be0fc51f834488581577155735
amal945 <amalsaji332@gmail.com>
2025-04-15 18:20:42 +0530

    readme.dart

---
+++ lib/readme.dart
@@ -0,0 +1,24 @@
+// packages used:
+//
+// get: ^4.7.2
+// local_auth: ^2.3.0
+//
+// Android Permissions
+// <uses-permission android:name="android.permission.USE_BIOMETRIC" />
+// <uses-permission android:name="android.permission.USE_FINGERPRINT" />
+// <uses-permission android:name="android.permission.INTERNET"/>
+//
+//
+// IOS Permissions
+//
+// <key>NSFaceIDUsageDescription</key>
+// <string>We use Face ID to authenticate you</string>
+//
+//
+// If using Kotlin (MainActivity.kt): at the top add  |package your.package.name| this line
+// for example in this project:
+// package com.example.bio_metrics ----> this line
+//
+// import io.flutter.embedding.android.FlutterFragmentActivity
+//
+// class MainActivity : FlutterFragmentActivity()
